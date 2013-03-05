2ce04538b000fd03b13abdf40d3efbafa8eac57e
Ryan Jacobs <rjacobs8888@gmail.com>
2013-03-04 23:57:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ryanterest::Application.config.secret_token = 'cd017e0bda7893d98a0bdf67a6b01b38bfe3f6ad97b2743a1d762aa880b9e00b6dd8f2046a7a46d81f81f063576498b224925ddaa59ab289f6ce36896aafe13a'
